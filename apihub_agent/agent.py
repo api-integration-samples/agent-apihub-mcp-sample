da3948792ed5d067707c027ad7031fe411ed033d
tyayers <tyler.ayers@gmail.com>
2025-05-28 12:57:30 +0200

    updated

--- apihub_agent/agent.py
+++ apihub_agent/agent.py
@@ -14,7 +14,7 @@ integration_tools = ApplicationIntegrationToolset(
     location="europe-west1",
     integration="test-flow",
     triggers=["api_trigger/test-flow_API_1"],
-    tool_instructions="This tool can add 23 to the variable input if it is greater than 50, or just add 18 if it is less than or equal to 50. The result is returned in the variable result."
+    tool_instructions="Enter an input number and get the result from an addition operation."
 )
 
 root_agent = Agent(
@@ -24,7 +24,7 @@ root_agent = Agent(
       "Agent to answer questions about Renewable energy APIs and web content. ."
   ),
   instruction=(
-      "You are a helpful agent who can answer user questions about renewable energy projects and parse web pages."
+      "You are a helpful agent who can answer user questions about renewable energy projects, parse web pages, and also do addition operations on input numbers."
   ),
   tools=[renewable_api_toolset, fetcher_tools, integration_tools],
 )
